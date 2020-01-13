d9e30ae067cf0e99edf61458e5572a8b62c51441
Annette Odingo <odingoannette@gmail.com>
2020-01-13 12:45:48 +0000

    Check if api is working

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // implement your API here
+const express = require('express')
+const cors = require('cors')
+
+
+const {find, findById, insert, update, remove} = require('./data/db')
+
+const app = express()
+
+app.use(express.json())
+
+app.use(cors())
+app.get('/', (req, res)=>{
+    res.json('this is the response')
+})
+
+app.listen(5000, ()=>{
+    console.log('listening on 5000')
+})
+
+
