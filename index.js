c6b3fefe1acaceb5c8f023274f928d3006dbe4f2
Annette Odingo <odingoannette@gmail.com>
2020-01-13 13:01:23 +0000

    Create api to fetch all user in db

--- index.js
+++ index.js
@@ -13,6 +13,15 @@ app.use(cors())
 app.get('/', (req, res)=>{
     res.json('this is the response')
 })
+app.get('/users', (req,res)=>{
+    find()
+    .then(users=>{
+        res.status(200).json(users)
+    })
+    .catch(error=>{
+        console.log(error.message)
+    })
+})
 
 app.listen(5000, ()=>{
     console.log('listening on 5000')
